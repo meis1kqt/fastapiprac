62cc268235227b44d33b620a045f06d6f2f8faab
pavel <pavelrz648@yandex.ru>
2026-01-14 16:46:24 +0300

    chore: ебал я в рот все что тут есть и каждого кто придумал эту хуйню до завтра пидорасы из гугл

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/meis1kqt/fastapiprac/internal/handlers"
+	"github.com/meis1kqt/fastapiprac/internal/storage"
+)
+
 func main() {
 	
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	if databaseURL == "" {
+		databaseURL = "postgres://taskuser:taskpass@localhost:5432/tasksdb?sslmode=disable"
+	}
+
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = "8080"
+	}
+
+	log.Printf("Starting server %s", serverPort)
+
+
+	db , err := storage.Connect(databaseURL)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	defer db.Close()
+
+	storage := storage.NewTaskStore(db)
+
+	handlers := handlers.NewHandlers(storage)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/tasks", handlers.GetAllTasks)
+	mux.HandleFunc("/tasks/", handlers.GetTaskByID)
+	mux.HandleFunc("/delete")
+
+
+
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,12 +1,51 @@
 package handlers
 
-import "github.com/meis1kqt/fastapiprac/internal/storage"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/meis1kqt/fastapiprac/internal/storage"
+)
 
 type Handlers struct {
 	store *storage.TaskStore
 }
 
 
-func NewHandlers (store *storage.TaskStore) *Handlers {
+func NewHandlers(store *storage.TaskStore) *Handlers {
 	return &Handlers{store: store}
 }
+
+func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "applicatiin/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func respondWithError(w http.ResponseWriter, status int, message string) {
+	respondWithJSON(w, status, map[string]string{"error": message})
+}
+
+
+func (h *Handlers) GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	tasks , err := h.store.GetAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve tasks")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, tasks)
+}	
+
+func (h *Handlers) GetTaskByID(w http.ResponseWriter, r *http.Request) {
+
+	task , err := h.store.GetById()
+
+	if err != nil {
+		log.Fatalf("failed")
+	}
+
+	respondWithJSON(w, http.StatusOK, task)
+}
+
